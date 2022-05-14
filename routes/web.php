21ba066d5fe349f8ed9385eff49b35913ac1780c
Jorgix <smith.exe@hotmail.com>
2022-05-14 10:43:11 -0400

    Arquivos de rota

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\SetorCadastro;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SetorCadastro::class, 'index'])->name('setor.index');
+//Rotas Painel
+Route::get('/painel', [SetorCadastro::class, 'index'])->name('painel.main');
+Route::get('/create/setor', [SetorCadastro::class, 'create'])->name('painel.create');
+Route::post('/create', [SetorCadastro::class, 'store'])->name('painel.store');
+
+
+//Rotas públicas
+Route::get('/', [DocumentController::class, 'index'])->name('doc.index');
+Route::get('/create/document', [DocumentController::class, 'create'])->name('doc.create');
+Route::post('/create', [DocumentController::class, 'store'])->name('doc.store');
+
+
 
 
