efe9e71e8439f9fdcee6b3729ff8675ffaa302f4
Jorgix <smith.exe@hotmail.com>
2022-05-14 10:25:59 -0400

    Controller responsável pelo cadastro / listagem e armazenamento dos setores

--- app/Http/Controllers/SetorCadastro.php
+++ app/Http/Controllers/SetorCadastro.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateSectorFormRequest;
 use App\Models\SetorCadastro as ModelsSetorCadastro;
 use Illuminate\Http\Request;
 
 class SetorCadastro extends Controller
 {
     public function index(){
-
         $setores = ModelsSetorCadastro::get();
-        return view('solasstech.index', compact('setores'));
+        return view('painel.main', compact('setores'));
     }
+
+    public function create(){
+        return view('painel.create');
+    }
+
+    public function store(StoreUpdateSectorFormRequest $request)
+    {
+        $data = $request->all();
+
+        SetorCadastro::create($data);
+
+        return redirect()
+                ->route('painel.main')
+                ->with('msg', 'Setor criado com sucesso');;
+    }
+
 }
