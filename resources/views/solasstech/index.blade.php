7cdcf5e97b85020ca7eb102583d431049339c16a
Jorgix <smith.exe@hotmail.com>
2022-05-14 10:45:13 -0400

    Página inicial, listagem dos documentos com botão de cadastro e formulário de busca.

--- resources/views/solasstech/index.blade.php
+++ resources/views/solasstech/index.blade.php
@@ -1,49 +1,61 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+@extends('solasstech.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ url(mix('solasstech/css/bootstrap.css')) }}">
-    <title>Listagem dos setores</title>
-</head>
+@section('title', 'Listagem dos setores')
 
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col col-lg-12 p-5">
-                <h1>Listagem dos setores</h1>
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Nº Documento</th>
-                            <th scope="col">Titulo</th>
-                            <th scope="col">Setor</th>
-                            <th scope="col">Data / Hora Envio</th>
-                            <th scope="col">Setor Recebeu</th>
-                            <th scope="col">Data e hora que recebeu</th>
-                            <th scope="col">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                @foreach ($setores as $setor)
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>{{$setor->Sigla}}</td>
-                                <td>@mdo</td>
-                                <td>@mdo</td>
-                                <td>@mdo</td>
-                                <td><button type="button" class="btn btn-outline-primary">Primary</button> <button type="button" class="btn btn-outline-secondary">Secundário</button></td>
-                            </tr>
-                @endforeach
-            </tbody>
-        </table>
-            </div>
+@section('content')
+
+    <div class="row ">
+        <div class="col-8">
+            <h1>Gerenciador de arquivos</h1>
+            <a href="{{ route('doc.create') }}" class="btn btn-primary my-3">Novo Documento</a>
+        </div>
+        <div class="col-4">
+
+            <form action="{{ route('doc.index') }}" method="get">
+                <div class="input-group">
+                    <div class="form-outline">
+                        <input type="search" name="search" class="form-control size-search"
+                            placeholder="Digite o Titulo ou Número" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Pesquisar</button>
+                </div>
+            </form>
+
         </div>
     </div>
 
-</body>
+    @if (session('msg'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('msg') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 
-</html>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">Nº Documento</th>
+                <th scope="col">Titulo</th>
+                <th scope="col">Setor</th>
+                <th scope="col">Data / Hora Envio</th>
+                <th scope="col">Setor Recebeu</th>
+                <th scope="col">Data e hora que recebeu</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($documents as $document)
+                <tr>
+                    <th scope="row">{{ $document->ndocument }}</th>
+                    <td>{{ $document->title }}</td>
+                    <td>SETOR</td>
+                    <td>{{ \Carbon\Carbon::parse($document->updated_at)->format('j \d\e F \d\e Y \à\s\ H:i') }} </td>
+                    <td>SETOR</td>
+                    <td>Hora que recebeu</td>
+                    <td><button type="button" class="btn btn-outline-success">Enviar</button> <button type="button"
+                            class="btn btn-outline-primary">Receber</button></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
