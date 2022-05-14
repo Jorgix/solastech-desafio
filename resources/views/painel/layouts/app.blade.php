417df3330b5a3ec2dc7ef77928536670f1b9eb53
Jorgix <smith.exe@hotmail.com>
2022-05-14 10:47:03 -0400

    Layout base da versão do painel

---
+++ resources/views/painel/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ url(mix('solasstech/css/bootstrap.css')) }}">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    @yield('content')
+
+
+</body>
+<script src="{{ url(mix('solasstech/js/bootstrap.bundle.js')) }}"></script>
+
+</html>
